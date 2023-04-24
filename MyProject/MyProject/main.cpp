89615320942319fea20db95652cd50b28bb275dc
minkyokyo <71928522+minkyokyo@users.noreply.github.com>
2023-04-24 18:18:35 +0900

    houghCircles

--- MyProject/MyProject/main.cpp
+++ MyProject/MyProject/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <opencv2/highgui.hpp>
 #include <opencv2/videoio.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
@@ -9,8 +10,12 @@ using namespace cv;
 int main(void)
 {
 	Mat img = imread("Lenna.png");
-
 	imshow("img", img);
 	waitKey(0);
+
+
+	vector<Vec3f> circles;
+	// https://diyver.tistory.com/102
+	HoughCircles();
 	return 0;
 }
